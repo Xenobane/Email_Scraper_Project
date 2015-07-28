a763c5e00b98d75b24be45f064bc9de58296c08d
Thomas J. Morris <manmorris@gmail.com>
2015-07-27 22:58:50 -0400

    General cleanup
    Improved comments, simplified function structure, and removed dead code.
    No feature changes.

--- find_email_addresses.py
+++ find_email_addresses.py
@@ -6,6 +6,14 @@ find_email_addresses.py
 This tool gathers all the email addresses it can discover on a website,
 and prints them in a list.
 
+TODO 1  fix get_emails to find emails anywhere, not just mailtos
+
+TODO 2  make get_links hungrier to traverse more than single relative links
+        (absolute links, multiple layers of relative links, query params...)
+        and design a smarter safety against runaway traversal
+        
+TODO 3  provide a better user experience (real time feedback, timeouts, URL resolution)        
+
 Author: Tom Morris
 Soundtrack: Deadmau5 - Get Scraped (2005)
 """
@@ -28,61 +36,47 @@ def find_email_addresses(url):
             
         allLinks, allEmails = visit(url, currentDepth, allLinks, allEmails)
 
-        #print()
-        #print("VISITED LINKS:")
-        #print(allLinks.sort())
-
-        #print("EMAILS:")
-        #print(allEmails.sort())
     except:
         print("Whoops! Caught an unhandled exception. This needs work... try a different URL!")
 
 def visit(link, currentDepth, visitedLinks, foundEmails):
     """Attempts to open a new link, searching it for new emails and other parts of the site."""
 
+    """Use current depth as a quick and dirty backstop against runaway traversal."""
     currentDepth += 1
-    
-    siteOpened = False
-    if currentDepth < MAX_DEPTH:
-    
-        #print("Opening " + link)
-        
-        try:
-            page = requests.get(link)
-            siteOpened = True
-        except:
-            if currentDepth == 1:
-                print("Oops, something went wrong with that URL! Please try a different URL.")
-            else:
-                print("DEBUG: Failed secondary link")
-        
-        if siteOpened:
-        
-            visitedLinks.append(link)
-            soup = BeautifulSoup(page.text, "lxml")
-            
-            """Strain any emails out of this page's soup"""
-            newEmails = getEmails(soup)
-            for email in newEmails:
-                if email not in foundEmails:
-                    print(email)
-                    foundEmails.append(email)
+    if currentDepth > MAX_DEPTH:
+        return visitedLinks, foundEmails
 
-            """Skim any new links out of the soup, and visit each one"""
-            newLinks = getLinks(soup, link, visitedLinks)
-            for link in newLinks:
-                if link not in visitedLinks:
-                    newVisitedLinks, newFoundEmails = visit(link, currentDepth, visitedLinks, foundEmails)
-                    visitedLinks.append(newVisitedLinks)
-                    foundEmails.append(newFoundEmails)
-    else:
-        #print("DEBUG: Encountered Max Depth of " + str(MAX_DEPTH))
-        pass
+    try:
+        page = requests.get(link)
+    except:
+        if currentDepth == 1:
+            print("Oops, something went wrong with that URL! Please try a different URL.")
+        else:
+            return visitedLinks, foundEmails
+    
+    visitedLinks.append(link)
+    soup = BeautifulSoup(page.text, "lxml")
+    
+    """Strain any emails out of this page's soup"""
+    newEmails = get_emails(soup)
+    for email in newEmails:
+        if email not in foundEmails:
+            print(email)
+            foundEmails.append(email)
+
+    """Skim any new links out of the soup, and visit each one"""
+    newLinks = get_links(soup, link, visitedLinks)
+    for link in newLinks:
+        if link not in visitedLinks:
+            newVisitedLinks, newFoundEmails = visit(link, currentDepth, visitedLinks, foundEmails)
+            visitedLinks.append(newVisitedLinks)
+            foundEmails.append(newFoundEmails)
         
     return visitedLinks, foundEmails
 
     
-def getEmails(someSoup):
+def get_emails(someSoup):
     """This function takes a BeautifulSoup object and returns a list of any emails that were found"""
     
     newEmails = []
@@ -96,14 +90,14 @@ def getEmails(someSoup):
     
     return newEmails
 
-def getLinks(someSoup, currentSite, visitedLinks):
+def get_links(someSoup, currentSite, visitedLinks):
     """This function returns a set of potential links that are believed to be part of this site."""
     
     newLinks = []
     
     for elem in someSoup.find_all('a'):
         
-        """...if that link is also "part of" this website..."""
+        """Figure out whether it's 'part of' this site."""
         validLink, link = is_link_part_of_site(currentSite, elem, visitedLinks)
         
         if validLink and link not in visitedLinks:
@@ -118,25 +112,20 @@ def is_link_part_of_site(currentSite, linkElem, visitedLinks):
     link = ""
     
     hrefString = linkElem.get('href');
-    
     if hrefString == None:
-        pass
-        
-    elif (len(hrefString) > 1) and (hrefString[0] == '/'):
+        return partOfSite, link
+    
+    """build an absolute link from relative links"""
+    if (len(hrefString) > 1) and (hrefString[0] == '/'):
         
         """avoid appending the same thing recursively"""
         suffix = False
         if hrefString == currentSite[-len(hrefString):]:
             suffix = True
         
-        #for vlink in visitedLinks:
-        #    if hrefString == vlink[-len(hrefString):]:
-        #        suffix = True
-        
         if suffix == False:
             partOfSite = True
             link = visitedLinks[0] + hrefString
-            #print(link + " is part of site!")
     
     return partOfSite, link
 
